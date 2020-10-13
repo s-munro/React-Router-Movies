25b425d58d64994508e57640a9f25112ac6ccb4a
s-munro <68410363+s-munro@users.noreply.github.com>
2020-10-12 21:51:32 -0400

    movies added on index, need fix movie detatails route

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function MovieList(props) {
-  return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="movie-list">{props.movies.map((movie) => <MovieDetails key={movie.id} movie={movie} />)}</div>
+	);
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+	const { title, director, metascore } = props.movie;
 
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
+	return (
+		<Link to={`/movies/${props.movie.id}`}>
+			<div className="movie-card">
+				<h2>{title}</h2>
+				<div className="movie-director">
+					Director: <em>{director}</em>
+				</div>
+				<div className="movie-metascore">
+					Metascore: <strong>{metascore}</strong>
+				</div>
+			</div>
+		</Link>
+	);
 }
